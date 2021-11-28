f3b8e122d993dfd9a49a599e5866418a316ee8b5
Andy <musictopia2@hotmail.com>
2021-11-28 07:25:16 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿partial class Program
+{
+    static void Main(string[] args)
+    {
+        HelloFrom("Generated Code");
+    }
+    static partial void HelloFrom(string name);
+}
---
+++ MyGenerator/MySourceGenerator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis;
+namespace MyGenerator
+{
+    [Generator]
+    public class MySourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // find the main method
+            var mainMethod = context.Compilation.GetEntryPoint(context.CancellationToken);
+
+            // build up the source code
+            string source = $@"
+using System;
+
+namespace {mainMethod.ContainingNamespace.ToDisplayString()}
+{{
+    public static partial class {mainMethod.ContainingType.Name}
+    {{
+        static partial void HelloFrom(string name)
+        {{
+            Console.WriteLine($""Generator says: Hi from '{{name}}'"");
+        }}
+    }}
+}}
+";
+            // add the source code to the compilation
+            context.AddSource("generatedSource", source);
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // No initialization required for this one
+        }
+    }
+}
